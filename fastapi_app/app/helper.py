1d7ef3c15a5395284dd26cfa6627dbcb34f9d81b
Natapon Pantuwong <pantuwongn@gmail.com>
2023-03-07 22:53:26 +0700

    add function to get workgroup, user, and task

--- fastapi_app/app/helper.py
+++ fastapi_app/app/helper.py
@@ -3,6 +3,7 @@ import requests
 import json
 from fastapi.security import APIKeyHeader
 from fastapi import HTTPException, Depends
+from typing import List, Dict
 
 from app.models import DepartmentModel, OrganizationModel
 
@@ -28,13 +29,87 @@ def api_key_auth(x_api_key: str = Depends(X_API_KEY)):
 def get_user( uid: str ) -> dict:
     ''' This function get user data from Bitrix24 backend from user id
     '''
-    url = f'{BASE_URL}/im.user.get.json?ID={uid}'     
+    url = f'{BASE_URL}/user.get.json?ID={uid}'     
     r = requests.get(url)
     rj = r.json()
     if r.status_code != 200 or 'result' not in rj:
         return None
     else:
         return rj['result']
+
+def get_user_in_dep( dep_id: str ) ->List[dict]
+    ''' This function returns all user in department with department id
+    '''
+    url = f'{BASE_URL}/user.get.json?order[ID]=ASC&UF_DEPARTMENT={dep_id}'
+    r = requests.get(url)
+    rj = r.json()
+    if r.status_code != 200 or 'result' not in rj:
+        return None
+    else:
+        all_users = rj['result']
+        while 'next' in rj:
+            last_id = all_users[-1]['ID']
+            url = f'{url}&filter[>ID]={last_id}'
+            r = requests.get(url)
+            rj = r.json()
+            if r.status_code != 200 or 'result' not in rj:
+                break
+            else:
+                all_users.extend(rj['result'])
+        return all_users
+
+def get_workgroup_in_dep( dep_id:str ) -> List[dict]
+    ''' This function returns a list of workgroup(project) in department
+    '''
+
+    # get user in department
+    all_users = get_user_in_dep( dep_id )
+    all_user_ids = [x['ID'] for x in all_users]
+
+    # query workgroup (project) where owner_id in all_user_ids
+    url = f'{BASE_URL}/sonet_group.get.json?order[ID]=ASC&OWNER_ID={all_user_ids}'
+    r = requests.get(url)
+    rj = r.json()
+    if r.status_code != 200 or 'result' not in rj:
+        return None
+    else:
+        all_workgroups = rj['result']
+        while 'next' in rj:
+            last_id = all_workgroups[-1]['ID']
+            url = f'{url}&filter[>ID]={last_id}'
+            r = requests.get(url)
+            rj = r.json()
+            if r.status_code != 200 or 'result' not in rj:
+                break
+            else:
+                all_workgroups.extend(rj['result'])
+        return all_workgroups
+
+def get_users_in_workgroup( wg_id:str ) -> List[dict]
+    ''' This function returns a list of users in department
+    '''
+
+    # get user data in workgroup
+    url = f'{BASE_URL}/sonot_group.user.get.json?ID={wg_id}&order[ID]=ASC'
+    r = requests.get(url)
+    rj = r.json()
+    if r.status_code != 200 or 'result' not in rj:
+        return None
+    else:
+        all_user_data = rj['result']
+        all_user_ids = [x['USER_ID'] for x in all_user_data]
+        url = f'{BASE_URL}/user.get.json?order[ID]=ASC&ID={all_user_ids}'
+        all_users = rj['result']
+        while 'next' in rj:
+            last_id = all_users[-1]['ID']
+            url = f'{url}&filter[>ID]={last_id}'
+            r = requests.get(url)
+            rj = r.json()
+            if r.status_code != 200 or 'result' not in rj:
+                break
+            else:
+                all_users.extend(rj['result'])
+        return all_users
 
 def get_organizations():
     ''' This function call Bitrix24 endpoints to get organization information
@@ -48,13 +123,23 @@ def get_organizations():
             dep_data_dict[dep_data['department_id']] = dep_data
 
     # get all departments
-    url = f'{BASE_URL}/department.get.json'
+    url = f'{BASE_URL}/department.get.json?order[ID]=ASC'
     r = requests.get(url)
     rj = r.json()
     if r.status_code != 200 or 'result' not in rj:
         return OrganizationModel(dep_list = None)
     else:
         all_deps = rj['result']
+        while 'next' in rj:
+            last_id = all_deps[-1]['ID']
+            url = f'{url}&filter[>ID]={last_id}'
+            r = requests.get(url)
+            rj = r.json()
+            if r.status_code != 200 or 'result' not in rj:
+                break
+            else:
+                all_deps.extend(rj['result'])
+
         dep_list = []
         for dep in all_deps:
             dep_id = dep.get('ID', None)

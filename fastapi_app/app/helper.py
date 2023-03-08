d024e1258b0df3c81afc42db67814fff20269460
Natapon Pantuwong <pantuwongn@gmail.com>
2023-03-08 11:56:54 +0700

    fix function to get workgroup and user

--- fastapi_app/app/helper.py
+++ fastapi_app/app/helper.py
@@ -22,7 +22,7 @@ def api_key_auth(x_api_key: str = Depends(X_API_KEY)):
     #   reject access
     if x_api_key != api_key:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=401,
             detail="Forbidden"
         )
 
@@ -37,10 +37,10 @@ def get_user( uid: str ) -> dict:
     else:
         return rj['result']
 
-def get_user_in_dep( dep_id: str ) ->List[dict]
+def get_user_in_dep( dep_id: str ) -> List[dict]:
     ''' This function returns all user in department with department id
     '''
-    url = f'{BASE_URL}/user.get.json?order[ID]=ASC&UF_DEPARTMENT={dep_id}'
+    url = f'{BASE_URL}/user.get.json?ORDER[ID]=ASC&UF_DEPARTMENT={dep_id}'
     r = requests.get(url)
     rj = r.json()
     if r.status_code != 200 or 'result' not in rj:
@@ -49,7 +49,7 @@ def get_user_in_dep( dep_id: str ) ->List[dict]
         all_users = rj['result']
         while 'next' in rj:
             last_id = all_users[-1]['ID']
-            url = f'{url}&filter[>ID]={last_id}'
+            url = f'{url}&FILTER[>ID]={last_id}'
             r = requests.get(url)
             rj = r.json()
             if r.status_code != 200 or 'result' not in rj:
@@ -58,7 +58,7 @@ def get_user_in_dep( dep_id: str ) ->List[dict]
                 all_users.extend(rj['result'])
         return all_users
 
-def get_workgroup_in_dep( dep_id:str ) -> List[dict]
+def get_workgroup_in_dep( dep_id:str ) -> List[dict]:
     ''' This function returns a list of workgroup(project) in department
     '''
 
@@ -67,30 +67,32 @@ def get_workgroup_in_dep( dep_id:str ) -> List[dict]
     all_user_ids = [x['ID'] for x in all_users]
 
     # query workgroup (project) where owner_id in all_user_ids
-    url = f'{BASE_URL}/sonet_group.get.json?order[ID]=ASC&OWNER_ID={all_user_ids}'
-    r = requests.get(url)
-    rj = r.json()
-    if r.status_code != 200 or 'result' not in rj:
-        return None
-    else:
-        all_workgroups = rj['result']
-        while 'next' in rj:
-            last_id = all_workgroups[-1]['ID']
-            url = f'{url}&filter[>ID]={last_id}'
-            r = requests.get(url)
-            rj = r.json()
-            if r.status_code != 200 or 'result' not in rj:
-                break
-            else:
-                all_workgroups.extend(rj['result'])
-        return all_workgroups
+    all_workgroups = []
+    for user_id in all_user_ids:
+        url = f'{BASE_URL}/sonet_group.get.json?ORDER[ID]=ASC&FILTER[OWNER_ID]={user_id}'
+        r = requests.get(url)
+        rj = r.json()
+        if r.status_code != 200 or 'result' not in rj:
+            continue
+        else:
+            all_workgroups.extend( rj['result'] )
+            while 'next' in rj:
+                last_id = all_workgroups[-1]['ID']
+                url = f'{url}&FILTER[>ID]={last_id}'
+                r = requests.get(url)
+                rj = r.json()
+                if r.status_code != 200 or 'result' not in rj:
+                    break
+                else:
+                    all_workgroups.extend(rj['result'])
+    return all_workgroups
 
-def get_users_in_workgroup( wg_id:str ) -> List[dict]
+def get_users_in_workgroup( wg_id:str ) -> List[dict]:
     ''' This function returns a list of users in department
     '''
 
     # get user data in workgroup
-    url = f'{BASE_URL}/sonot_group.user.get.json?ID={wg_id}&order[ID]=ASC'
+    url = f'{BASE_URL}/sonet_group.user.get.json?ID={wg_id}&ORDER[ID]=ASC'
     r = requests.get(url)
     rj = r.json()
     if r.status_code != 200 or 'result' not in rj:
@@ -98,7 +100,8 @@ def get_users_in_workgroup( wg_id:str ) -> List[dict]
     else:
         all_user_data = rj['result']
         all_user_ids = [x['USER_ID'] for x in all_user_data]
-        url = f'{BASE_URL}/user.get.json?order[ID]=ASC&ID={all_user_ids}'
+        url = f'{BASE_URL}/user.get.json?ORDER[ID]=ASC&ID={all_user_ids}'
+        print(url)
         all_users = rj['result']
         while 'next' in rj:
             last_id = all_users[-1]['ID']
@@ -123,22 +126,13 @@ def get_organizations():
             dep_data_dict[dep_data['department_id']] = dep_data
 
     # get all departments
-    url = f'{BASE_URL}/department.get.json?order[ID]=ASC'
+    url = f'{BASE_URL}/department.get.json'
     r = requests.get(url)
     rj = r.json()
     if r.status_code != 200 or 'result' not in rj:
         return OrganizationModel(dep_list = None)
     else:
         all_deps = rj['result']
-        while 'next' in rj:
-            last_id = all_deps[-1]['ID']
-            url = f'{url}&filter[>ID]={last_id}'
-            r = requests.get(url)
-            rj = r.json()
-            if r.status_code != 200 or 'result' not in rj:
-                break
-            else:
-                all_deps.extend(rj['result'])
 
         dep_list = []
         for dep in all_deps:
@@ -149,12 +143,21 @@ def get_organizations():
             dep_head_id = dep_data_dict[dep_id].get('manager_id', None)
             dep_head_pos = dep_data_dict[dep_id].get('position_text', None)
             parent = dep.get('PARENT', None)
-            dep_head = get_user(dep_head_id)
+            dep_head = get_user(dep_head_id)[0]
             if not dep_head:
                 dep_head_name = None
             else:
-                dep_head_name = dep_head.get('name', None)
-            
+                f_name = dep_head.get('NAME', None)
+                l_name = dep_head.get('LAST_NAME', None)
+                if f_name and l_name:
+                    dep_head_name = f'{f_name} {l_name}'
+                elif f_name:
+                    dep_head_name = f_name
+                elif l_name:
+                    dep_head_name = l_name
+                else:
+                    dep_head_name = None
+
             dep_obj = DepartmentModel(dep_id = dep_id,
                                         dep_name = dep_name,
                                         dep_head_id = dep_head_id,
